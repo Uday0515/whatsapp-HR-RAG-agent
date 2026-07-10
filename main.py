5f286d516f5cff65bfe322e7db81c39a4f7538bc
UDAY KUMAR R <1ds23ri058@dsce.edu.in>
2026-07-10 15:31:15 +0530

    Whatsapp integration via Twilio + cloudFlare Tunnel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import Response
 from pydantic import BaseModel
 from rag.generator import generate_answer
 
@@ -13,3 +14,17 @@ class Question(BaseModel):
 def ask(question: Question):
     answer = generate_answer(question.query)
     return {"answer": answer}
+
+
+@app.post("/whatsapp")
+async def whatsapp_webhook(request: Request):
+    form = await request.form()
+    incoming_msg = form.get("Body")
+    answer = generate_answer(incoming_msg)
+
+    twiml = f"""<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Message>{answer}</Message>
+</Response>"""
+
+    return Response(content=twiml, media_type="application/xml")
